884e76f6e565431017414540e1ead44de595751f
lwh <2391519868@qq.com>
2018-08-09 21:59:33 +0800

    增加2题

---
+++ src/lwh/codeguide/chp5/RotateWord.java
@@ -0,0 +1,29 @@
+package lwh.codeguide.chp5;
+
+import java.util.Scanner;
+
+public class RotateWord {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNextLine()){
+            String str1 = sc.nextLine();
+            String str2 = sc.nextLine();
+
+            System.out.println(isRotateWord(str1, str2));
+        }
+    }
+
+    private static boolean isRotateWord(String str1, String str2) {
+        if(str1 == null || str2 == null){
+            return false;
+        }
+
+        str1 = str1 + str1;
+
+        if(str1.contains(str2)){
+            return true;
+        }
+        return false;
+    }
+}
---
+++ src/lwh/codeguide/chp5/StringToInteger.java
@@ -0,0 +1,68 @@
+package lwh.codeguide.chp5;
+
+import java.util.Scanner;
+
+public class StringToInteger {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNextLine()){
+            String str = sc.nextLine();
+            System.out.println(parseInt(str));
+        }
+    }
+
+    private static int parseInt(String str) {
+        if(!isValid(str)){
+            return 0;
+        }
+
+        char[] chs = str.toCharArray();
+        boolean flag = chs[0] == '-' ? false : true;
+
+        int minq = Integer.MIN_VALUE / 10;
+        int minr = Integer.MIN_VALUE % 10;
+
+        int res = 0;
+        int cur = 0;
+
+        for(int i = flag ? 0 : 1; i < chs.length; i++){
+            cur = '0' - chs[i];
+            if(res < minq || (res == minq && cur < minr)){
+                return 0;
+            }
+            res = res * 10 + cur;
+        }
+
+        if(flag && res == Integer.MIN_VALUE){
+            return 0;
+        }
+
+        return flag ? -res : res;
+    }
+
+    private static boolean isValid(String str){
+        if(str == null || str.length() == 0){
+            return false;
+        }
+
+        char[] chs = str.toCharArray();
+        if(chs[0] != '-' && (chs[0] > '9' || chs[0] < '0')){
+            return false;
+        }
+        if(chs[0] == '-' && (chs.length == 1 || chs[1] == '0')){
+            return false;
+        }
+        if(chs[0] == '0' && chs.length > 1){
+            return false;
+        }
+
+        for(int i = 1; i < chs.length; i++){
+            if(chs[i] > '9' || chs[i] < '0'){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
