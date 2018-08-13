60620aa4a705aa91be0fdedeeb80e66fac9ece2f
lwh <2391519868@qq.com>
2018-08-13 22:00:14 +0800

    增加2题

---
+++ src/lwh/PointOffer/Problem10_Fibonaqi.java
@@ -0,0 +1,57 @@
+package lwh.PointOffer;
+
+public class Problem10_Fibonaqi {
+
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(getFibonaci(n));
+    }
+
+    private static int getFibonaci(int n) {
+        if(n < 1){
+            return 0;
+        }
+
+        if(n == 1 || n == 2){
+            return 1;
+        }
+
+        int[][] base = {{1, 1},{1, 0}};
+        int[][] res = matrixPower(base, n - 2);
+
+        return res[0][0] + res[1][0];
+    }
+
+    private static int[][] matrixPower(int[][] m, int p) {
+        int[][] res = new int[m.length][m[0].length];
+
+        for(int i = 0; i < res.length; i++){
+            res[i][i] = 1;
+        }
+
+        int[][] tmp = m;
+
+        for(; p != 0; p >>= 1){
+            if( (p & 1) != 0){
+                res = matrixMultiply(res, tmp);
+            }
+            tmp = matrixMultiply(tmp, tmp);
+        }
+
+        return res;
+    }
+
+    private static int[][] matrixMultiply(int[][] p, int[][] q) {
+        int[][] res = new int[p.length][q[0].length];
+
+        for(int i = 0; i < p.length; i++){
+            for(int j = 0; j < q[0].length; j++){
+                for(int k = 0; k < q.length; k++){
+                    res[i][j] += p[i][k] * q[k][j];
+                }
+            }
+        }
+
+        return res;
+    }
+}
---
+++ src/lwh/PointOffer/Problem11_RotateArray.java
@@ -0,0 +1,21 @@
+package lwh.PointOffer;
+
+public class Problem11_RotateArray {
+
+    public int minNumberInRotateArray(int [] array) {
+        if(array == null || array.length == 0){
+            return 0;
+        }
+
+        int min = array[0];
+
+        for(int i = 0; i < array.length - 1; i++){
+            if(array[i] > array[i+1]){
+                min = array[i+1];
+                break;
+            }
+        }
+
+        return min;
+    }
+}
