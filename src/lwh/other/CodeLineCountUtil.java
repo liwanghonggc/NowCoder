ae99c6a3751326f25175e0924d234836e7d47de1
lwh <2391519868@qq.com>
2018-08-17 17:02:41 +0800

    增加读取代码行数方法

---
+++ src/lwh/other/CodeLineCountUtil.java
@@ -0,0 +1,49 @@
+package lwh.other;
+
+import java.io.*;
+
+/**
+ * 读取代码行数
+ */
+public class CodeLineCountUtil {
+
+    private static int count = 0;
+
+    public static void main(String[] args) {
+        String path = "D:\\Test";
+        File file = new File(path);
+        int lineCount = getLineCountOfCode(file);
+        System.out.println(lineCount);
+    }
+
+    private static int getLineCountOfCode(File file) {
+        if(file.isFile()){
+            count += getFileCount(file);
+        }else{
+            File[] files = file.listFiles();
+            for(File f : files){
+                getLineCountOfCode(f);
+            }
+        }
+        return count;
+    }
+
+    public static int getFileCount(File file){
+        int num = 0;
+        String name = file.getName();
+        if(name != null && name.endsWith(".java")){
+            try {
+                FileInputStream fis = new FileInputStream(file);
+                InputStreamReader isr = new InputStreamReader(fis);
+                BufferedReader br = new BufferedReader(isr);
+                while((br.readLine()) != null){
+                    num++;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.out.println("File " + name + " is count: " + num);
+        }
+        return num;
+    }
+}
