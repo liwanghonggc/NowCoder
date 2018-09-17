11aff20b8f809bae58840045f977a3829a0d224c
lwh <2391519868@qq.com>
2018-09-17 13:56:11 +0800

    增加1题

--- src/lwh/PointOffer/Problem1_DuplicateNumInArray.java
+++ src/lwh/PointOffer/Problem1_DuplicateNumInArray.java
@@ -15,25 +15,24 @@ public class Problem1_DuplicateNumInArray {
 
 	/**
 	 * 返回重复数字的下标
-	 * @param arr
+	 * @param num
 	 * @return
 	 */
-	public static int duplicate(int[] arr) {
-		if(arr == null || arr.length < 2){
+	public static int duplicate(int[] num) {
+		if(num == null || num.length < 2){
 			return -1;
 		}
 
-		for(int i = 0; i < arr.length; i++){
-			while(arr[i] != i){
-				if(arr[i] == arr[arr[i]]){
+		for(int i = 0; i < num.length; i++){
+			while(num[i] != i){
+				if(num[i] == num[num[i]]){
 					return i;
 				}
-				int temp = arr[i];
-				arr[i] = arr[temp];
-				arr[temp] = temp;
+				int temp = num[i];
+				num[i] = num[temp];
+				num[temp] = temp;
 			}
 		}
-
 		return -1;
     }
 
---
+++ src/lwh/PointOffer/Problem9_TwoStackGetQueue.java
@@ -0,0 +1,47 @@
+package lwh.PointOffer;
+
+import java.util.Stack;
+
+/**
+ * @author lwh
+ * @date 2018-09-17
+ * @desp
+ */
+public class Problem9_TwoStackGetQueue {
+
+    private static final Stack<Integer> stack1 = new Stack<>();
+
+    private static final Stack<Integer> stack2 = new Stack<>();
+
+    /**
+     * 入队操作,直接将元素放到stack1中
+     * @param num
+     */
+    public void addToTail(Integer num){
+        stack1.push(num);
+    }
+
+    /**
+     * 出队操作,若stack2非空,直接从Stack2中弹出一个元素;若Stack2为空,将stack1中元素依此弹出放入stack2中
+     * @return
+     */
+    public Integer getNumFormHead(){
+        if(stack2.isEmpty()){
+            while(!stack1.isEmpty()){
+                int num = stack1.pop();
+                stack2.push(num);
+            }
+        }
+        return stack2.pop();
+    }
+
+    public static void main(String[] args) {
+        Problem9_TwoStackGetQueue queue = new Problem9_TwoStackGetQueue();
+        queue.addToTail(1);
+        queue.addToTail(2);
+        queue.addToTail(3);
+
+        System.out.println(queue.getNumFormHead());
+        System.out.println(queue.getNumFormHead());
+    }
+}
