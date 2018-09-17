061b19131db9e61c7ca05e8a7fe14f8b46fdfea3
lwh <2391519868@qq.com>
2018-09-17 11:01:25 +0800

    增加1题

--- src/lwh/offer/DuplicateNumInArray.java
+++ src/lwh/offer/DuplicateNumInArray.java
@@ -3,32 +3,38 @@ package lwh.offer;
 public class DuplicateNumInArray {
 
 	public static void main(String[] args) {
-		int[] a = {4,7,3,1,0,2,6,5};
-		int[] duplication = new int[a.length];
-		boolean flag = duplicate(a, a.length, duplication);
-		if(flag){
-			System.out.println(duplication[0]);
+		int[] arr = {4,7,3,1,0,1,6,5};
+		int index = duplicate(arr);
+		if(index == -1){
+			System.out.println("不存在重复数字");
 		}else{
-			System.out.println("No duplicate num!");
+			System.out.println(arr[index]);
 		}
+
 	}
-	
-	public static boolean duplicate(int numbers[],int length,int [] duplication) {
-	    if(numbers == null || length == 0){
-	    	return false;
-	    }
-	    Integer[] table = new Integer[length];
-	    for(int i = 0;i < length; i++){
-	    	int index = numbers[i];
-	    	if(table[index] == null){
-	    		table[index] = index;
-	    	}else{
-	    		duplication[0] = table[index];
-	    		return true;
-	    	}
-	    	
-	    }
-	    return false;
+
+	/**
+	 * 返回重复数字的下标
+	 * @param arr
+	 * @return
+	 */
+	public static int duplicate(int[] arr) {
+		if(arr == null || arr.length < 2){
+			return -1;
+		}
+
+		for(int i = 0; i < arr.length; i++){
+			while(arr[i] != i){
+				if(arr[i] == arr[arr[i]]){
+					return i;
+				}
+				int temp = arr[i];
+				arr[i] = arr[temp];
+				arr[temp] = temp;
+			}
+		}
+
+		return -1;
     }
 
 }
