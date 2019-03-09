f80c21130936d7f84491840021ac498eb080ec17
lwh <2391519868@qq.com>
2019-03-09 18:05:19 +0800

    斐波那契数列O(logN)解法

--- src/lwh/codeguide/chp4/Fibonaqi.java
+++ src/lwh/codeguide/chp4/Fibonaqi.java
@@ -9,7 +9,7 @@ public class Fibonaqi {
 
     public static void main(String[] args) {
         for(int i = 0; i < 10; i++){
-            System.out.println(fibonaqi1(i) + ", " + fibonaqi2(i));
+            System.out.println(fibonaqi1(i) + ", " + fibonaqi2(i) + ", " + fibonaqi3(i));
         }
     }
 
@@ -38,6 +38,49 @@ public class Fibonaqi {
             pre = p;
             p = res;
         }
+        return res;
+    }
+
+    private static int fibonaqi3(int n){
+        if(n <= 0){
+            return 0;
+        }
+        if(n == 1 || n == 2){
+            return 1;
+        }
+
+        int[][] base = {{1, 1}, {1, 0}};
+        int[][] res = matrixPower(base, n - 2);
+        return res[0][0] + res[1][0];
+    }
+
+    private static int[][] matrixPower(int[][] m, int p){
+        int[][] res = new int[m.length][m[0].length];
+        for(int i = 0; i < m.length; i++){
+            res[i][i] = 1;
+        }
+
+        int[][] tmp = m;
+        for(; p != 0; p >>= 1){
+            if((p & 1) != 0){
+                res = multiMatrix(res, tmp);
+            }
+            tmp = multiMatrix(tmp, tmp);
+        }
+
+        return res;
+    }
+
+    private static int[][] multiMatrix(int[][] m1, int[][] m2){
+        int[][] res = new int[m1.length][m2[0].length];
+        for(int i = 0; i < m1.length; i++){
+            for(int j = 0; j < m2[0].length; j++){
+                for(int k = 0; k < m2.length; k++){
+                    res[i][j] += m1[i][k] * m2[k][j];
+                }
+            }
+        }
+
         return res;
     }
 }
